fe5f138acdce49bf733ce660f61452811b0bfa95
Ken Rutan <kenr16@gmail.com>
2017-05-02 02:39:28 -0700

    add necessary additional features to the page.

--- app.rb
+++ app.rb
@@ -32,3 +32,12 @@ get("/lists/:id") do
   @list = List.find(params.fetch("id").to_i())
   erb(:list)
 end
+
+post("/tasks") do
+  description = params.fetch("description")
+  list_id = params.fetch("list_id").to_i()
+  @list = List.find(list_id)
+  @task = Task.new({:description => description, :list_id => list_id})
+  @task.save()
+  erb(:task_success)
+end
